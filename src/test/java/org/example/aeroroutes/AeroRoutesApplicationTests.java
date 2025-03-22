7d7f6560b0394da0de07b15526419db9c20b0b26
cakimaki <dani.tsakovski3@gmail.com>
2025-03-22 23:37:08 +0200

    Created Spring Boot project with Java 21

---
+++ src/test/java/org/example/aeroroutes/AeroRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aeroroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroRoutesApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
