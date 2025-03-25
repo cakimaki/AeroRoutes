4567bb1703c1a01c7b8736e62a75f7cd4d188563
cakimaki <dani.tsakovski3@gmail.com>
2025-03-25 14:32:12 +0200

    Minor improvement in the repository class
    Improvements of the RepositoryImpl:
    - Adding final as once declared it's not changed.
    - Changed the order of the methods (more readable for me)

--- src/main/java/org/example/aeroroutes/repository/FlightRepositoryImpl.java
+++ src/main/java/org/example/aeroroutes/repository/FlightRepositoryImpl.java
@@ -7,17 +7,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
 public class FlightRepositoryImpl implements FlightRepository{
-	private List<Flight> flights;
+	private final List<Flight> flights;
 	
 	public FlightRepositoryImpl(){
 		this.flights = new ArrayList<>();
 		loadFlights();
+	}
+	
+	@Override
+	public List<Flight> getAllFlights() {
+		return flights;
+	}
+	
+	@Override
+	public List<Flight> getFlightsFrom(String origin) {
+		return flights.stream()
+				.filter(flight -> flight.getOrigin().equalsIgnoreCase(origin))
+				.collect(Collectors.toCollection(ArrayList::new));
 	}
 	
 	private void loadFlights(){
@@ -26,7 +39,7 @@ public class FlightRepositoryImpl implements FlightRepository{
 			throw new RuntimeException("flights.txt not found in resources");
 		}
 		
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));){
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));){
 			String line;
 			
 			while((line = br.readLine()) != null){
@@ -46,17 +59,5 @@ public class FlightRepositoryImpl implements FlightRepository{
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-	}
-	
-	@Override
-	public List<Flight> getAllFlights() {
-		return flights;
-	}
-	
-	@Override
-	public List<Flight> getFlightsFrom(String origin) {
-		return flights.stream()
-				.filter(flight -> flight.getOrigin().equalsIgnoreCase(origin))
-				.collect(Collectors.toCollection(ArrayList::new));
 	}
 }
